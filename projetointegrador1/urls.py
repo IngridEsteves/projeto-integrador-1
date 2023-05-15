54820befac1d7f1f52300bf3677a48b1ec5ce9e5
IngridEsteves <ingridestevespereiraiep@gmail.com>
2023-05-14 23:18:20 -0300

    relatorios-part1

--- projetointegrador1/urls.py
+++ projetointegrador1/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('autorizacao/', include('autorizacao.urls')),
+    path('relatorios/', include('relatorios.urls')),
 ]
